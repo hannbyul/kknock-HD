ed1a9f822ca63712d78bd48dc270429d77371e1d
Younghun Kwon <kyonggi@kakao.com>
2022-07-12 01:32:27 +0900

    close #2 apm 설치 함수 생성

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# apm 설치 함수
+apm_setting() { 
+    sudo apt update & sudo apt upgrade 
+
+    # apache2, php, mysql 설치
+    sudo apt install apache2 php php-mysql mysql-server -y
+
+    # apache2 실행
+    sudo service apache2 start
+}
